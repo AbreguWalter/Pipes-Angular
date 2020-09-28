65faff84f61cc06a2642970a080624c7a42ddcd7
Walter Abregu <w.abregu@comunal.co>
2020-09-27 23:57:11 -0500

    pipes 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,28 @@ export class AppComponent {
   // tslint:disable-next-line: no-inferrable-types
   nombre: string = 'Capital America';
 
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  PI: number = Math.PI;
+
+  // tslint:disable-next-line: no-inferrable-types
+  porcertanje: number = 0.235;
+
+  salario = 1234.5;
+
+  valorPromesa = new Promise<string>( (resolve) => {
+    setTimeout(() => {
+      resolve('llego la data');
+    }, 4500);
+  });
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'wolverin',
+    edad: 500,
+    direccion : {
+      calle: 'Primera',
+      casa: 20
+    }
+  };
 }
